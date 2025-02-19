936db368fefca40afe4a8ec439b7c1a23c63cbf8
rimrim <rimrimrimrim1987@gmail.com>
2025-02-19 18:08:23 +0800

    inserted model into data

--- titanic.py
+++ titanic.py
@@ -1,24 +1,23 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
 
-from __scripts__.data import DataType, clean_data
-from __scripts__.model import ColumnTransformer
+from __scripts__.data import ColumnTransformer, DataType, clean_data
 
 df = pd.read_csv("titanic/train.csv")
 
-ddf = clean_data(df, drop_missing=False, drop_uninformative=True)
+df = clean_data(df, drop_missing=False, drop_uninformative=True)
 
-dtype_dict = DataType.infer_df_dtype(ddf)
+dtype_dict = DataType.infer_df_dtype(df)
 dtype_dict["Survived"] = dtype_dict["Survived"].set_ordinal()
 
 # check_corr(ddf, "Survived", dtype_dict)
 
 X_trans, Y_trans = ColumnTransformer.create_transformers(
-    ddf,
+    df,
     dtype_dict,
     labels="Survived",
 )
-X_scaled, I_trans = X_trans.fit_transform(ddf, imputer=SimpleImputer())
+X_scaled, I_trans = X_trans.fit_transform(df, imputer=SimpleImputer())
 X_revert = X_trans.inverse_transform_df(X_scaled)
 
 
